a9c24a0b5055833d45dfb22aff538d27d762c047
Asim Mehmood Khan <57537718+AsimKhan2019@users.noreply.github.com>
2022-09-12 20:49:34 +0500

    Created schema.sql
    The schema file contains the SQL command to create the animals' table in the database.

---
+++ schema.sql
@@ -0,0 +1,22 @@
+-- Table: animals
+
+-- DROP TABLE IF EXISTS animals;
+
+CREATE TABLE IF NOT EXISTS animals
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2000 CACHE 1 ),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weigth double precision NOT NULL,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS animals
+    OWNER to admin;
+
+COMMENT ON TABLE animals
+    IS 'A microverse exercise to create an animal table.';
